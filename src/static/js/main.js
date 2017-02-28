033e986ae547198ead3f716d20c90fc78eaf18fe
andysellick <andysellick@builtvisible.com>
2017-02-28 13:25:35 +0000

    Tweaking the way the JS works

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,1 +1,21 @@
-hljs.initHighlightingOnLoad();
+
+
+//given an array of ids, start the weather plugin on those elements
+function makeItRain(elements){
+	var options = {
+		speed:getRandomInt(3,10),
+		dropDensity:getRandomInt(5,20),
+		wind:getRandomInt(-10,10),
+		dropHeight:1,
+		dropColor:[0,0,0]
+	};
+	for(var i = 0; i < elements.length; i++){
+		new weatherObj(document.getElementById(elements[i]),options);
+	}
+}
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
